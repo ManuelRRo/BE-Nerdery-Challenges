e1fc4d30f20edbb9cbc353861ad27864d243c529
ManuelRRo <ds23906@gmail.com>
2025-04-15 10:58:48 -0600

    time-difference pass tests

--- 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
@@ -16,6 +16,44 @@ timeDifference(7200, 3400); // Expected output: "01:03:20"
 
 */
 
-const timeDifference = (a, b) => {};
+// format HH:MM:SS.
+
+const timeDifference = (a, b) => {
+    let hours,minutes,seconds;
+    let txt_hours,txt_minutes,txt_seconds;
+    const ABSOLUTE_DIFFERENCE = Math.abs(a-b);
+
+    hours = Math.floor(((ABSOLUTE_DIFFERENCE)/3600));
+    minutes = Math.floor(ABSOLUTE_DIFFERENCE / 60) % 60;
+    seconds = ABSOLUTE_DIFFERENCE % 60;
+
+    if(hours === 0){
+        txt_hours = '00';
+    }else if(hours < 10) {
+        txt_hours = '0'+hours;
+    }
+    else{
+        txt_hours = hours;
+    }
+
+    if(seconds === 0){
+        txt_seconds = '00';
+    }else if (seconds < 10){
+        txt_seconds = '0'+ seconds;
+    }else{
+        txt_seconds = seconds;
+    }
+
+    if(minutes===0){
+        txt_minutes = '00';
+    }else if (minutes < 10){
+        txt_minutes = '0'+minutes;
+    }
+    else{
+        txt_minutes = minutes;
+    }
+    
+    return `${txt_hours}:${txt_minutes}:${txt_seconds}`;
+};
 
 module.exports = timeDifference;
