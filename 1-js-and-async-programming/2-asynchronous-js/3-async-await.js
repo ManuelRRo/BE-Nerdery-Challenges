649958b35880845fa0a24829fd3e060d4f438952
ManuelRRo <ds23906@gmail.com>
2025-04-16 16:20:04 -0600

    3-asyn-await.js donde

--- 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
+++ 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
@@ -17,8 +17,58 @@
  *
  * @returns {Promise<string>} Logs the subscription name as a string.
  */
+const mocked_api = require("./utils/mocked-api");
 const getCommonDislikedSubscription = async () => {
   // Add your code here
+  const userPromise = mocked_api.getUsers();
+  const likeMoviesPromise = mocked_api.getLikedMovies();
+  const dislikeMoviesPromise = mocked_api.getDislikedMovies();
+
+  // async function subscriptionByUser(id_user) {
+
+  //   let user_promise = await mocked_api.getUserSubscriptionByUserId(id_user);
+
+  //   console.log("funcion"+typeof(user_promise));
+
+  //   return user_promise.subscription;
+  // }
+
+  const [users,likes,dislikes] = await Promise.all([
+    userPromise,
+    likeMoviesPromise,
+    dislikeMoviesPromise
+  ]);
+  //console.log(users,likes,dislikes,subscriptionByUser);
+
+  dislikesMap = new Map();
+  dislikes.forEach((element) => {
+    dislikesMap.set(element.userId, element.movies.length);
+  });
+
+  const likesMap = new Map();
+  likes.forEach(element => {
+    likesMap.set(element.userId, element.movies.length);
+  });
+
+  let basicCount=0,premiumCount=0;
+  for (const user of users) {
+    if (dislikesMap.get(user.id) > likesMap.get(user.id)) {
+      const value = await mocked_api.getUserSubscriptionByUserId(user.id);
+      if(value.subscription === "Basic"){
+        basicCount++;
+      }else{
+        premiumCount++;
+      }
+    }
+  }
+  
+  if(basicCount > premiumCount){
+    return "Basic"
+  }else{
+    return "Premium"
+  }
+
+
 };
 
 getCommonDislikedSubscription().then((subscription) => {
