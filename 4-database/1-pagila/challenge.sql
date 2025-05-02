a14071e9249f85b051950285d553c2facc27f8ee
ManuelRRo <ds23906@gmail.com>
2025-05-02 15:49:20 -0600

    pagila challenge and banking challenge done

--- 4-database/1-pagila/challenge.sql
+++ 4-database/1-pagila/challenge.sql
@@ -11,8 +11,9 @@ SELECT
     c.name AS category,
     COUNT(*) AS film_count
 FROM
-    public.category c
-    INNER JOIN public.film_category fc ON fc.category_id = c.category_id
+    public.category c INNER JOIN public.film_category fc 
+    ON 
+    fc.category_id = c.category_id
 GROUP BY
     c.name;
 
@@ -30,8 +31,9 @@ SELECT
     c.last_name,
     SUM(p.amount) as total_spent
 FROM
-    public.payment p
-    INNER JOIN public.customer c ON p.customer_id = c.customer_id
+    public.payment p INNER JOIN public.customer c 
+ON 
+    p.customer_id = c.customer_id
 GROUP BY
     c.customer_id
 ORDER BY
@@ -47,8 +49,22 @@ Write a SQL query that lists all film titles that have not been rented in the pa
 - The time period for "recent" should be within the last 10 years from the current date
 - Results should only include films that have no rental records in this time period
  */
-
-    -- your query here
+-- your query here
+SELECT
+    f.title
+FROM
+    public.film f
+WHERE
+    f.film_id not in (
+        SELECT
+            i.film_id
+        FROM
+            public.inventory i JOIN public.rental r 
+        ON 
+            i.inventory_id = r.inventory_id
+        WHERE
+            r.rental_date >= current_date - interval '10 years'
+    );
     /*
     Challenge 4.
     Write a SQL query that lists all films that have never been rented in the Pagila database.
@@ -63,13 +79,14 @@ WITH
             i.film_id,
             i.inventory_id
         FROM
-            public.rental r
-            FULL OUTER JOIN public.inventory i ON r.inventory_id = i.inventory_id
+            public.rental r FULL OUTER JOIN public.inventory i 
+        ON 
+            r.inventory_id = i.inventory_id
         WHERE
-            i.inventory_id not in (
-                select
+            i.inventory_id NOT IN (
+                SELECT
                     inventory_id
-                from
+                FROM
                     rental
             )
         GROUP BY
@@ -80,9 +97,9 @@ SELECT
     f.title,
     fwnr.inventory_id
 FROM
-    public.film f
-    INNER JOIN films_with_no_rentals fwnr ON f.film_id = fwnr.film_id
-
+    public.film f INNER JOIN films_with_no_rentals fwnr 
+ON 
+    f.film_id = fwnr.film_id;
     /*
     Challenge 5.
     Write a SQL query that lists all films that were rented more times than the average rental count per film in the Pagila database.
@@ -91,28 +108,109 @@ FROM
     - rental_count should show the total number of times the film was rented
      */
     -- your query here
+WITH movies_count_rented AS(
+
+	SELECT 
+        i.film_id,
+        COUNT(*) as times_rented
+	FROM
+        public.rental r INNER JOIN public.inventory i
+	ON 
+        r.inventory_id = i.inventory_id
+	GROUP BY 
+        i.film_id
+	ORDER BY 
+        i.film_id
+
+), average_count_rented AS(
+SELECT 
+	AVG(times_rented)::numeric(10,0) AS average_rental_count 
+FROM 
+	movies_count_rented
+)
+SELECT 
+    f.title, mcr.times_rented
+FROM 
+    public.film f JOIN movies_count_rented mcr
+ON 
+    f.film_id=mcr.film_id
+WHERE 
+    times_rented > ( SELECT * FROM average_count_rented)
+ORDER BY 
+    mcr.times_rented DESC;
+
     /*
-    Challenge 6.
-    Write a SQL query that calculates rental activity for each customer.
-    - The query should return the customer's first_name and last_name
-    - It should also return their first rental date as first_rental
-    - Their most recent rental date should be shown as last_rental
-    - The difference in days between the first and last rentals should be shown as rental_span_days
-    - Results should be grouped by customer and ordered by rental_span_days in descending order
-     */
-    -- your query here
-    /*
-    Challenge 7.
-    Find all customers who have not rented movies from every available genre.
-    - The result should include the customer's first_name and last_name
-    - Only include customers who are missing at least one genre in their rental history
-     */
-    -- your query here
-    /*
-    Bonus Challenge 8 (opt)
-    Find the Top 3 Most Frequently Rented Films in Each Category and Their Total Rental Revenue.
-    - The result should include the film title, category name, rental count, and total revenue
-    - Only the top 3 films per category should be returned
-    - Results should be ordered by category and ranking within the category
-     */
-    -- your query here
+Challenge 6.
+Write a SQL query that calculates rental activity for each customer.
+- The query should return the customer's first_name and last_name
+- It should also return their first rental date as first_rental
+- Their most recent rental date should be shown as last_rental
+- The difference in days between the first and last rentals should be shown as rental_span_days
+- Results should be grouped by customer and ordered by rental_span_days in descending order
+    */
+-- your query here
+SELECT 
+    c.first_name,
+    c.last_name,
+    MIN(r.rental_date) AS first_rental,
+    MAX(r.rental_date) AS last_rental,
+    DATE_PART('day', MAX(r.rental_date) - MIN(r.rental_date)) AS rental_span_days
+FROM 
+    public.customer c
+JOIN 
+    public.rental r ON c.customer_id = r.customer_id
+GROUP BY 
+    c.customer_id, c.first_name, c.last_name
+ORDER BY 
+    rental_span_days DESC;
+
+/*
+Challenge 7.
+Find all *customers who have not rented movies from *every available genre*.
+- The result should include the customer's first_name and last_name
+- Only include customers who are missing at least one genre in their rental history
+    */
+-- your query here
+
+WITH total_count_of_genres AS (
+	SELECT 
+		COUNT(*) AS genre_count
+	FROM 
+		public.category cat
+), customer_genre_rented AS (
+	SELECT
+	    r.customer_id,
+		COUNT(DISTINCT fc.category_id) AS genre_rented
+	FROM 
+		public.rental r JOIN public.inventory i
+	ON 
+		r.inventory_id = i.inventory_id JOIN public.film_category fc
+	ON 
+		i.film_id = fc.film_id
+	GROUP BY 
+		r.customer_id
+	ORDER BY 
+		r.customer_id
+)
+SELECT 
+    c.first_name,
+    c.last_name
+FROM 
+    public.customer c JOIN customer_genre_rented cgr
+ON 
+    c.customer_id = cgr.customer_id
+WHERE cgr.genre_rented < (
+	SELECT *
+	FROM total_count_of_genres
+)
+ORDER BY 
+	c.last_name,
+    c.first_name
+/*
+Bonus Challenge 8 (opt)
+Find the Top 3 Most Frequently Rented Films in Each Category and Their Total Rental Revenue.
+- The result should include the film title, category name, rental count, and total revenue
+- Only the top 3 films per category should be returned
+- Results should be ordered by category and ranking within the category
+    */
+-- your query here
